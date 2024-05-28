5385817d1ed1a6edf60953b1f6f11d9cfa1c0019
mselvi <mselvi@iski.gov.tr>
2024-05-28 09:53:35 +0300

    list

---
+++ src/main/java/com/melik/JavaExercise/core/collections/list/ArrayListExample.java
@@ -0,0 +1,37 @@
+package com.melik.JavaExercise.core.collections.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author mselvi
+ * @Created 28.05.2024
+ */
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void filterAction() {
+        /*
+        * Burada list yapısını sadece first yapısındaki elemanları içerecek şekilde filtreledik.
+        * */
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        first.add(2);
+        first.add(3);
+        first.add(4);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.retainAll(first);
+        list.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/melik/JavaExercise/core/collections/list/ArrayListVsLinkedList.java
@@ -0,0 +1,50 @@
+package com.melik.JavaExercise.core.collections.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author mselvi
+ * @Created 28.05.2024
+ */
+
+public class ArrayListVsLinkedList {
+
+    public static void main(String[] args) {
+
+        List<Integer> list=new ArrayList<>();
+
+        /*
+         * The big-o notation of element addition is O(1) or O(N)
+         * O(1) validate when we are trying to add elements to arraylist that has not reached first initial capacity.
+         * O(N) validate when we are trying to add elements to arraylist that needs to expand the array that is uses in the background
+         * */
+        list.add(1);
+        list.add(2);
+
+        /*
+        * The big-o notation of element search is O(1).
+        * Because we don't need to iterate all list
+        * */
+        list.contains(1);
+        list.indexOf(2);
+
+        List<Integer> linkedList=new LinkedList<>();
+
+        /*
+         * The big-o notation of element addition is O(1)
+         * Because we don't have any capacity for our LinkedList as with ArrayList
+         * */
+        linkedList.add(1);
+        linkedList.add(2);
+
+
+        /*
+         * The big-o notation of element search is O(N).
+         * Because we have to iterate all list when we are trying to add elements to linkedlist
+         * */
+        linkedList.contains(1);
+        linkedList.indexOf(1);
+    }
+}
