6ae8ecb0c6aaac9e0dc96fa99e32f15e69230bf8
Meliksah Selvi <meliksahselvi.engineer@gmail.com>
2026-02-22 15:28:41 +0300

    udemy

---
+++ src/main/java/com/melik/JavaExercise/coding/livecoding/Udemy.java
@@ -0,0 +1,17 @@
+package com.melik.JavaExercise.coding.livecoding;
+
+public class Udemy {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private int otherFunc(int result){
+        return 0;
+    }
+
+    private int calculateResult(int number) {
+        return number % 2 == 0 ? number / 2 : 3 * number + 1;
+    }
+}
